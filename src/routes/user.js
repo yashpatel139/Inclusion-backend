c20f590c03de8b89395c1fbaef55437e5fad3029
Bhoopendra Singh <bhoopendrasingh2306@gmail.com>
2024-07-30 12:34:13 +0530

    forget password api

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,7 +3,7 @@ const validator = require("validator");
 const passwordValidator = require("password-validator");
 const router = express.Router();
 const user = require("../db/user");
-const contactUs = require("../db/contactus");
+// const contactUs = require("../db/contactus");
 
 //http://localhost:5000/user/signup
 router.post("/signup", async (req, resp) => {
@@ -25,7 +25,7 @@ router.post("/signup", async (req, resp) => {
   try {
     const { name, email, password, confirmpassword } = req.body;
     const result = {
-        name: name,
+      name: name,
       email: email,
       password: password,
       confirmpassword: confirmpassword,
@@ -48,13 +48,15 @@ router.post("/signup", async (req, resp) => {
     let userCheck = await user.findOne({ email: email });
     console.log("uZER chack:", userCheck);
     if (userCheck) {
-      return resp.status(203).send({ message: "user already exists" });
+      return resp.status(203).send({ message: "User already exists." });
     } else {
       let valu = new user(result);
       let data = await valu.save();
       data = data.toObject();
       //   resp.status(200).send({ data, auth: Token });
-      resp.status(200).send({ data });
+      resp
+        .status(200)
+        .send({ data, message: "sign up successful , Please login now." });
     }
   } catch (err) {
     console.log("end Err", err);
@@ -86,7 +88,6 @@ router.post("/login", async (req, res) => {
   }
 });
 
-
 //http://localhost:5000/user/contactus
 
 // router.post("/contactus", async (req, res) => {
@@ -107,10 +108,69 @@ router.post("/login", async (req, res) => {
 //       console.log(err);
 //       res.status(500).send({ message: "Error in sending message" });
 //     }
-  
 // });
 
+//http://localhost:5000/user/forgetpassword
 
+router.post("/forgetpassword", async (req, res) => {
+  const { email } = req.body;
+  try {
+    const user = await user.findOne({ email: email });
+    if (!user) {
+      return res
+        .status(203)
+        .send({ message: "No user is registered with this email" });
+    } else {
+      return res
+        .status(200)
+        .send({ message: "User is registered", email: email });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(203).send({ message: "Error in sending message" });
+  }
+});
 
+//http://localhost:5000/user/passwordupdate
+router.put("/passwordupdate", async (req, res) => {
+  const schema = new passwordValidator();
+  schema
+    .is()
+    .min(8) // Minimum length 8
+    .is()
+    .max(100) // Maximum length 100
+    .has()
+    .uppercase() // Must have uppercase letters
+    .has()
+    .lowercase() // Must have lowercase letters
+    .has()
+    .digits() // Must have digits
+    .has()
+    .not()
+    .spaces();
+  const { email, password, confirmpassword } = req.body;
+  try {
+    if (!validator.isEmail(email)) {
+      return res.status(203).send({ message: "Invalid email address" });
+    }
+    //password validation check
+    const isValid = schema.validate(password);
+    if (!isValid) {
+      return res.status(203).send({ message: "Password is not valid" });
+    }
+    if (password !== confirmpassword) {
+      return res
+        .status(203)
+        .send({ message: "Password and confirm password does not match" });
+    }
+    var query = { email: email };
+    var data = { password: password, confirmpassword: confirmpassword };
+    const result = await user.updateOne(query, data);
+    res.status(200).send({result, message:"Password updated successfully"});
+  } catch (err) {
+    console.log(err);
+    res.status(203).send({ message: "Error in resetting password" });
+  }
+});
 
 module.exports = router;
